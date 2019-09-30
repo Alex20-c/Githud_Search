baff7e7772db4fcbfcd231d51d74d26d015a051f
Alex Kiprop <kipropalex59@gmail.com>
2019-09-30 11:13:01 +0300

    Created file

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageErrorComponent } from './page-error/page-error.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileRequestComponent } from './profile-request/profile-request.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NavbarComponent,
+    PageErrorComponent,
+    ProfileComponent,
+    ProfileRequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
