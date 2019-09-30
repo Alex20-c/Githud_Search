84430bffb80dd65d65bd257eccd5b899e653dacb
Alex Kiprop <kipropalex59@gmail.com>
2019-09-30 13:12:05 +0300

    export class from profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import { ProfileService } from '../profile-request/profile.service.service';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  providers:[ProfileService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user:any=[];
+  
+  repo:any=[];
+  username:string=''
 
-  constructor() { }
+  constructor(private http:HttpClient,private profService:ProfileService) { 
+    
+  }
+  
+  getRepo(){
+    let promise=new Promise((resolve,reject)=>{
+      this.profService.getRepo(this.username).toPromise().then(response=>{
+        this.repo=response
+        
+        resolve()
+      },
+      error=>{
+        reject(error)
+      }
+      )
+    })
+   return promise
+  }
+  
+  
 
   ngOnInit() {
   }
