317395bf0417799a65016fd03a62b7c7ba4cca09
Alex Kiprop <kipropalex59@gmail.com>
2019-09-30 17:51:34 +0300

    import httpclient and ngmodule profilecomponent and uppercasepipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PageErrorComponent } from './page-error/page-error.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ProfileRequestComponent } from './profile-request/profile-request.component';
 import { UpperCasePipe } from './upper-case.pipe';
 
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageErrorComponent } from './page-error/page-error.component';
+import { ProfileService } from './profile-request/profile.service.service';
+import { ProfileComponent } from './profile/profile.component';
+import { RoutingModule } from './routing/routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    HomeComponent,
     NavbarComponent,
     PageErrorComponent,
     ProfileComponent,
-    ProfileRequestComponent,
     UpperCasePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
