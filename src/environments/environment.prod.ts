8224e844a693c42954eb5c0d18ecbfe43990c4bb
Alex Kiprop <kipropalex59@gmail.com>
2019-10-01 08:32:35 +0300

    add the api token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  access_token:'?access_token=9d009beaa4b463e55802dd242bf74776009c6ca6',
+  apiUrl: 'https://api.github.com',
 };
